b56f8ead164da5373c20db04152c713f7d912334
Josphat <musyokajosphat10@gmail.com>
2025-09-07 21:34:44 +0300

    added the frontend domain to cors

--- djbackend/settings.py
+++ djbackend/settings.py
@@ -165,7 +165,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://localhost:8081",
     "http://192.168.158.80:8080",
-    "http://127.0.0.1:8080"
+    "http://127.0.0.1:8080",
+    "https://celve.netlify.app"
 ]
 #CORS_ALLOWED_ORIGINS =True
 csrf_trusted_origins=[
